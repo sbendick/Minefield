34fb4aefb4bdf28189673cafca1434f91e34bc16
Steven Bendick <sbendick@mail.csuchico.edu>
2016-09-26 17:46:48 -0700

    final commit

--- FieldTest.cpp
+++ FieldTest.cpp
@@ -1,24 +1,64 @@
 /**
  * Unit Tests for Field class
-**/
+ **/
 
 #include <gtest/gtest.h>
 #include <iostream>
 #include "Field.h"
- 
+
 class FieldTest : public ::testing::Test
 {
-	protected:
-		FieldTest(){}
-		virtual ~FieldTest(){}
-		virtual void SetUp(){}
-		virtual void TearDown(){}
+    protected:
+        FieldTest(){}
+        virtual ~FieldTest(){}
+        virtual void SetUp(){}
+        virtual void TearDown(){}
 };
 
 TEST(FieldTest, placeMineInBounds)
 {
-	Field minefield;
-	
-	minefield.placeMine(4,5);
-	ASSERT_EQ( MINE_HIDDEN, minefield.get(4,5) );
+    Field minefield;
+
+    minefield.placeMine(4,5);
+    ASSERT_EQ( MINE_HIDDEN, minefield.get(4,5) );
+    minefield.placeMine(9,9);
+    ASSERT_EQ( MINE_HIDDEN, minefield.get(9,9) );
+    minefield.placeMine(0,0);
+    ASSERT_EQ( MINE_HIDDEN, minefield.get(0,0) );
+    minefield.placeMine(0,9);
+    ASSERT_EQ( MINE_HIDDEN, minefield.get(0,9) );
+    minefield.placeMine(9,0);
+    ASSERT_EQ( MINE_HIDDEN, minefield.get(9,0) );
+}
+
+TEST(FieldTest, constructorwithoutArgument)
+{
+    Field minefield;
+
+    minefield.placeMine(9, 9);
+    ASSERT_EQ( MINE_HIDDEN, minefield.get(9, 9) );
+}
+
+TEST(FieldTest, constructorWithArgument)
+{
+    Field minefield(20);
+
+    minefield.placeMine(19, 19);
+    ASSERT_EQ( MINE_HIDDEN, minefield.get(19, 19) );
+}
+
+TEST(FieldTest, RevealAdjacentAtEdge)
+{
+    Field minefield;
+
+    minefield.placeMine(4,5);
+    ASSERT_EQ( MINE_HIDDEN, minefield.get(4,5) );
+    minefield.placeMine(9,9);
+    ASSERT_EQ( MINE_HIDDEN, minefield.get(9,9) );
+    minefield.placeMine(0,0);
+    ASSERT_EQ( MINE_HIDDEN, minefield.get(0,0) );
+    minefield.placeMine(0,9);
+    ASSERT_EQ( MINE_HIDDEN, minefield.get(0,9) );
+    minefield.placeMine(9,0);
+    ASSERT_EQ( MINE_HIDDEN, minefield.get(9,0) );
 }
--- buglog.md
+++ buglog.md
@@ -1,1 +1,3 @@
 # BUG LOG
+
+[ FAILED ] FieldTest.constructorWithArgument // This test fails because when you call the constructor with an argument (20 in this case) it will makes the map the size 20X20, but it doesn't store the size in the variable "FIELD_DIMENSION", especially since it is declared const. It fails when placing a mine at  a location that should be valid, such as (17, 17),  because it checks against FIELD_DIMENSION, which is set to 10 by default.
